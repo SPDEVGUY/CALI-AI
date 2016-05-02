8ad24f1128993a61813ebc1320eaff2154e9da2b
Kevin Cole <kevin.cole@devfacto.com>
2016-05-02 00:46:47 -0700

    stuff

---
+++ Logic.Common/Processors/WhereIsMonikers.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+using CALI.Logic.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CALI.Database.Contracts.Data;
+using CALI.Logic.Common.Util;
+
+namespace CALI.Logic.Common.Processors
+{
+    public class WhereIsMonikers : ProcessorBase
+    {
+        public WhereIsMonikers() :
+            base(
+            @"(where|Where) (are|is) (the|an|a)* *(.+)",
+            SortOrderEnum.GeneralQuery,
+            "Where is DevFacto?",
+            "Where is the LSA?",
+            "Where is Kevin?"
+            )
+        { }
+
+        public override List<BinaryDataContract> Process(string query, MatchCollection matches)
+        {
+            using (new LogBlock(query))
+            {
+                var result = new List<BinaryDataContract>();
+
+                var items = Tester.Matches(query);
+                var groups = items[0].Groups;
+
+                var monikers = new List<MonikerContract>();
+                monikers.AddRange(MonikerRetriever.FindMonikers(groups[4].Value));
+
+                result = BinaryDataRetriever.GetData(monikers.ToArray());
+                return result;
+            }
+        }
+    }
+}
